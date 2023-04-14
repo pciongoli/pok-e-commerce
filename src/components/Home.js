d24f879811b144684de2d45ef15d0580724ded96
pcion <patrick.ciongoli@gmail.com>
2023-04-14 13:08:35 -0700

    set up basic components and routes

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+   return (
+      <div>
+         <h1>Welcome to the Pok-é-Commerce Marketplace!</h1>
+      </div>
+   );
+}
+
+export default Home;
